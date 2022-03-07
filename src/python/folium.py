b10647de7535c44a9c9448fe24e6b85b8f0545f5
Khom <mgrprinc37@gmail.com>
2022-03-07 20:28:52 +0545

    ikhom: folium in python

---
+++ src/python/folium.py
@@ -0,0 +1,21 @@
+# @KhomZ
+
+# import folium
+
+# map = folium.Map(location=[132.401867, -111.112322])
+# map.save('map.html')
+
+
+# today I'm gonna show you how folium can be used in python
+# pip install folium
+
+# Instant map can be drawn
+
+
+# lets get started
+
+import folium
+
+map = folium.Map(location=[32.401867, -111.112322])
+
+map.save('map1.html')
