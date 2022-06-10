ae99b33746f70519f87e3c61ed66116711de10a9
Khom <mgrprinc37@gmail.com>
2022-06-10 20:10:16 +0545

    ikhom: ML ANdrew Ng featureNormalize.m

--- machine-learning/week2/codes/programming-assignment-solutions-khom/ml-prog-assignment1/ex1/featureNormalize.m
+++ machine-learning/week2/codes/programming-assignment-solutions-khom/ml-prog-assignment1/ex1/featureNormalize.m
@@ -7,10 +7,8 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % You need to set these values correctly
 X_norm = X;
-mu = mean(X);
-sigma = std(X);
-X = (X - mu)./sigma;
-X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
@@ -29,9 +27,9 @@ X_norm = X;
 %       
 
 
-
-
-
+mu = mean(X);
+sigma = std(X);
+X_norm = (X - mu)./sigma;
 
 
 
